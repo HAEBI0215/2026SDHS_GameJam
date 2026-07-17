1e16587e5879bac9d4e12a548afd93b75dcf0f3b
kjw0316 <t01062656190@gmail.com>
2026-07-18 01:06:24 +0900

    괜히 사운드 어쩌고 했네

--- EoseOobSyo/Assets/Scripts/Manager/SoundManager.cs
+++ EoseOobSyo/Assets/Scripts/Manager/SoundManager.cs
@@ -2,14 +2,71 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager Instance;
+
+    public AudioSource audioSource;
+
+    // BGM
+    [Header("BGM")]
+    public AudioClip bgm;
+
+    // Cooking
+    [Header("Cooking Sounds")]
+    public AudioClip cookingSound;
+    public AudioClip slicingSound;
+    public AudioClip makeFoodSound;
+    public AudioClip failSound;
+
+    // Food Interaction
+    [Header("Food Interaction Sounds")]
+    public AudioClip grabSound;
+    public AudioClip setPlateSound;
+    public AudioClip wasteFoodSound;
+
+    // UI / System
+    [Header("UI & System Sounds")]
+    public AudioClip receiptSound;
+    public AudioClip coinSound;
+
+    // Result
+    [Header("Result Sounds")]
+    public AudioClip finishSound;
+
+    // Environment
+    [Header("Environment Sounds")]
+    public AudioClip openDoorSound;
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void PlayBGM(AudioClip clip)
     {
+        audioSource.clip = bgm;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
 
+    public void BGMVolume(float value)
+    {
+        audioSource.volume = value;
     }
 
     public void PlaySFX(AudioClip clip)
     {
+        audioSource.PlayOneShot(clip);
+    }
 
+    public void SFXVolume(float value)
+    {
+        audioSource.volume = value;
     }
 }
